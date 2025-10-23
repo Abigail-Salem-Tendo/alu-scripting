8200fadf945cabfd2ae0b91b0a7ab9f154b106b4
Abigail Salem Tendo <a.tendo@alustudent.com>
2025-10-23 08:44:30 +0000

    taskk one

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,6 +7,16 @@ This function creates a request to get the number of subcribers from the reddit 
 import requests
 
 
-def number_of_subcribers(subreddit):
-    url = "https://www.reddit.com/dev/api/{}".format(subreddit)
-    headers = {
+def number_of_subscribers(subreddit):
+    """
+    return the muber of subcribers of a subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {}
+    response = requests.get(url, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json()
+    return data.get("data", {}).get("subscribers", 0)
